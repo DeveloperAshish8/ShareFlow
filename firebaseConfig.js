1c4658fa52a88fbb3df394c0be0a45064d3abfd1
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-03-14 02:00:26 +0530

    slight configuration changes
    Firebase Configuration chnages

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,10 @@ const firebaseConfig = {
   storageBucket: "shareflow-web.appspot.com",
   messagingSenderId: "927792002059",
   appId: "1:927792002059:web:ca05251ab117a9ad08c30e",
-  measurementId: "G-368JDXG8DD"
+  measurementId: "G-368JDXG8DD",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null));
