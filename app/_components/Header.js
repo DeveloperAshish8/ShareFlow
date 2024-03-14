caa5a78034b65a687cc79261977676ef3b3ec9e4
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-03-14 19:02:26 +0530

    Header Responsiveness

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,50 +1,64 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import ".././globals.css";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [menuOpened, setOpened] = useState(false);
+  const handleClick = () => {
+    setOpened(!menuOpened);
+  };
   return (
     <header className="bg-transparent">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4  lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
             <Image src="./logo.svg" width={40} height={40} />
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
-            <nav aria-label="Global" className="hidden md:block">
-              <ul className="flex items-center gap-20 text-sm">
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    About{" "}
-                  </Link>
-                </li>
+            {/* <nav
+              aria-label="Global"
+              className={menuOpened ? "mobile-menu-link" : "menu-link"}
+            > */}
+            <ul
+              className={menuOpened ? "mobile-menu-link" : "menu-link"}
+              // menuOpened
+              //   ? ""
+              //   : "flex-col absolute gap-5 mt-14 bg-white px-14 py-10 text-lg rounded-xl"
+            >
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="#"
+                >
+                  {" "}
+                  About{" "}
+                </Link>
+              </li>
 
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/upload"
-                  >
-                    {" "}
-                    Upload{" "}
-                  </Link>
-                </li>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/upload"
+                >
+                  {" "}
+                  Upload{" "}
+                </Link>
+              </li>
 
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#HowTo"
-                  >
-                    {" "}
-                    How to{" "}
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="#HowTo"
+                >
+                  {" "}
+                  How to{" "}
+                </Link>
+              </li>
+            </ul>
+            {/* </nav> */}
 
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
@@ -57,7 +71,10 @@ const Header = () => {
               </div>
 
               <div className="block md:hidden">
-                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <button
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                  onClick={() => handleClick()}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
